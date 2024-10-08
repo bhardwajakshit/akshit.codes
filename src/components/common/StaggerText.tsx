f4dd7d4cc90366ef378fa47a70fa08d66810e209
bhardwajakshit <bhardwajakshit78@gmail.com>
2024-10-09 01:13:57 +0530

    chore: updated animation speed and header component

--- src/components/common/StaggerText.tsx
+++ src/components/common/StaggerText.tsx
@@ -30,7 +30,7 @@ const StaggerText = ({
       opacity: 1,
       y: 0,
       transition: {
-        duration: 0.5,
+        duration: 0.6,
       },
     },
   };
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,6 +9,7 @@ export const Header = ({
   updateCursor: (cursorUrl: string) => void;
 }) => {
   const { theme } = useTheme();
+  const lightTheme = theme === Theme.Light;
 
   return (
     <motion.div className="flex items-center justify-between w-full">
@@ -33,10 +34,10 @@ export const Header = ({
           <span
             className="py-1.5 bg-clip-text text-transparent hover:text-white dark:hover:text-transparent bg-gradient-to-r from-indigo-600 dark:from-[#cf4729] via-purple-600 dark:via-yellow-500 to-pink-600 dark:bg-red-700 animated-gradient transition-colors duration-300"
             onMouseEnter={() => {
-              theme === Theme.Light && updateCursor("/kickflip.ani");
+              lightTheme && updateCursor("/kickflip.ani");
             }}
             onMouseLeave={() => {
-              theme === Theme.Light && updateCursor("./smoke.ani");
+              lightTheme && updateCursor("./smoke.ani");
             }}
           >
             magic!
